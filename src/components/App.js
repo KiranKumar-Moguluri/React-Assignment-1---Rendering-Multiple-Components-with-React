7ef22d0410157bf178aa67813d6a2f55e19eb81b
Stack_Pointer <kiranchamp.6457@gmail.com>
2021-03-14 16:21:18 +0530

    About Project

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,20 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+   
+    <div className="Main">
+      <div data-ns-test="project-name">
+      <h1>Master room calculus</h1>
+    </div>
+    
+    <div data-ns-test="project-description">
+      <p>
+        This describes about operation accurately and can perform multi-operations at the same time.
+      </p>
+    </div>
+
+
+    </div>
   )
 }
 
